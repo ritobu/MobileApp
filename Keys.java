c2f090c24141d6771c830cd5e8b82cfdfcd1e2de
ritobu <bizzaresaiko99@gmail.com>
2017-06-12 20:17:33 +0700

    Add files via upload

---
+++ Keys.java
@@ -0,0 +1,14 @@
+package com.grontol.pakartanamanobat.utility;
+
+/**
+ * Created by GrT on 12/22/2016.
+ */
+
+public class Keys
+{
+    public static final String TITLE = "title";
+    public static final String ITEM_PERTANYAAN = "itemPertanyaan";
+    public static final String ITEM_PENYAKIT = "itemPenyakit";
+    public static final String ITEM_TANAMAN = "itemTanaman";
+    public static final String GAMBAR = "gambar";
+}
